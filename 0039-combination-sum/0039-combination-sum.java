109ee9d4e6d00d373bdc8930eabe4cb89caaf58f
Samiran Saha <131834156+samiran4663@users.noreply.github.com>
2025-07-09 12:56:57 +0530

    Time: 1 ms (99.91%), Space: 44.6 MB (81.48%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<Integer> list1=new ArrayList<>();
+        List<List<Integer>> ls= new ArrayList<>();
+        int indx=0;
+        reccomsum(candidates,list1,ls,indx,target);
+        return ls;
+    }
+    public void reccomsum(int [] candidates, List<Integer>list1,List<List<Integer>> ls,int indx,int target)
+    {
+        if(indx==candidates.length)
+        {
+            if(target==0){
+                ls.add(new ArrayList<>(list1));
+            }
+            return ;
+        }
+        if(candidates[indx]<=target){
+            list1.add(candidates[indx]);
+            reccomsum(candidates,list1,ls,indx,target-candidates[indx]);
+            list1.remove(list1.size()-1);
+        }
+
+        reccomsum(candidates,list1,ls,indx+1,target);
+    }
+}
