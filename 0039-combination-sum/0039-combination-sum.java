c41385ba4dbee4f8fdc5edf627b0a45ac642354e
Samiran Saha <131834156+samiran4663@users.noreply.github.com>
2025-07-25 20:30:28 +0530

    Time: 1 ms (99.91%), Space: 44.4 MB (97.09%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -8,19 +8,20 @@ class Solution {
     }
     public void reccomsum(int [] candidates, List<Integer>list1,List<List<Integer>> ls,int indx,int target)
     {
-        if(indx==candidates.length)
-        {
-            if(target==0){
+       if(indx==candidates.length)
+       {
+            if(target==0)
+            {
                 ls.add(new ArrayList<>(list1));
             }
-            return ;
+            return;
         }
-        if(candidates[indx]<=target){
+        if(candidates[indx]<=target)
+        {
             list1.add(candidates[indx]);
             reccomsum(candidates,list1,ls,indx,target-candidates[indx]);
             list1.remove(list1.size()-1);
         }
-
         reccomsum(candidates,list1,ls,indx+1,target);
     }
 }
