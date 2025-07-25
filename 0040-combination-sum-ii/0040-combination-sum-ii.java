c5db85253020a8792aa7ec79de9f78cc6b787c32
Samiran Saha <131834156+samiran4663@users.noreply.github.com>
2025-07-25 22:22:48 +0530

    Time: 3 ms (72.86%), Space: 43.1 MB (94.94%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -10,18 +10,20 @@ class Solution {
     }
     public void comsum2(int candidates[],int target,int indx,List<Integer> list1,List<List<Integer>>ls)
     {
-        if(target==0)
-        {
-            ls.add(new ArrayList<>(list1));
-            return;
-        }
+       if(target==0)
+            {
+                ls.add(new ArrayList<>(list1));
+                return;
+            }
         for(int i=indx;i<candidates.length;i++)
         {
             if(i>indx && candidates[i]==candidates[i-1]) continue;
             if(candidates[i]>target) break;
+
             list1.add(candidates[i]);
             comsum2(candidates,target-candidates[i],i+1,list1,ls);
             list1.remove(list1.size()-1);
         }
+        
     }
 }
