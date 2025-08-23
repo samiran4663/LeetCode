7dde30e070212de6be28090b1dcf6e7714237e21
Samiran Saha <131834156+samiran4663@users.noreply.github.com>
2025-08-23 11:45:43 +0530

    Time: 4 ms (11.24%), Space: 41.8 MB (66.14%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,30 +1,29 @@
 class Solution {
     public boolean isValid(String s) {
         Deque<Character> stack = new ArrayDeque<>();
-        if(s.charAt(0)==')'||s.charAt(0)=='}'||s.charAt(0)==']')return false;
         for(int i=0;i<s.length();i++)
         {
             if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='[')
             {
                 stack.push(s.charAt(i));
             }
-            else if(!stack.isEmpty())
+            else
             {
-                if((stack.peek()=='(' && s.charAt(i)!=')')||(stack.peek()=='{' && s.charAt(i)!='}')
-                || (stack.peek()=='[' && s.charAt(i)!=']'))
+                if(stack.isEmpty())return false;
+                else
                 {
-                    return false;
+                    if((stack.peek()=='(' && s.charAt(i)!=')')||(stack.peek()=='{' && s.charAt(i)!='}')
+                    || (stack.peek()=='[' && s.charAt(i)!=']'))
+                    {
+                        return false;
+                    }
+                    else{
+                        stack.pop();
+                    }
                 }
-                else{
-                    stack.pop();
-                }
-            }
-            else if(stack.isEmpty())
-            {
-                return false;
             }
         }
         
-        return stack.isEmpty()?true:false;
+        return stack.isEmpty();
     }
 }
